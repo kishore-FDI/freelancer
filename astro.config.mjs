9718374ecb3224d6c5d0593ca2e8d3c0ebae4b91
Kishore <kishore22705@gmail.com>
2026-09-07 00:52:01 +0530

    Remaking

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
-
+import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
+	  vite: {
+		plugins: [tailwindcss()],
+	},
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Kishore Kumar';
+export const SITE_DESCRIPTION = 'Statmaxxing in my 20s';
+
+export const NAVBAR_ITEMS = {
+    "Blog" : "blog",
+	"Work": "work",
+    "Now" : "now",
+};
